b9f3524a1b283e98bcbda7318a68930544b830cd
yamunahdev-711 <dinidevi711@gmail.com>
2024-05-13 16:01:17 +0800

    hello

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://app.netlify.com/sites/soft-banoffee-4c8ba3/overview",
+  integrations: [preact()]
+});
